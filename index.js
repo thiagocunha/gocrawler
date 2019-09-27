8380bc922c51ca0ac61ac8836db2b933633c3315
Thiago Chierici Cunha <tchierici@lnxcit019242.cit>
2019-09-27 08:48:42 -0300

    recursive, ordered version with status code validation

--- index.js
+++ index.js
@@ -5,8 +5,31 @@ var cheerio = require('cheerio'); // Basically jQuery for node.js
 var listaNormal = [];
 var listaErro = [];
 
+function execRequests(r, fulldomain, body, listaNormal, listaErro, callback){
+    if ((result = r.exec(body)) !== null) {
+        let item = decodeURIComponent(result[1]);
+        //console.log(item);
+        if (item.startsWith("/")){
+            item = fulldomain + item;
+        }
+        if (item.startsWith("http") && !listaNormal.includes(item)&& !listaErro.includes(item)){
+            listaNormal.push(item);
+            //console.log(item);
+            recReq(item, listaNormal, listaErro, function(listaNormal, listaErro){
+                execRequests(r, fulldomain, body, listaNormal, listaErro, callback);
+            });            
+        }
+        else{
+            execRequests(r, fulldomain, body, listaNormal, listaErro, callback);
+        }        
+        
+    }
+    else{
+        callback(listaNormal, listaErro);
+    }
+}
 
-function recReq(url, listaRef, listaErro, cb){
+function recReq(url, listaNormal, listaErro, cb){
     var options = {
         uri: url,
         transform: function (body) {
@@ -19,30 +42,23 @@ function recReq(url, listaRef, listaErro, cb){
         //console.log(".");
         //console.log(response);
 
-        console.log(response.statusCode);
-        if (parseInt(response.statusCode) !== 200){
+        //console.log(response.statusCode);
+        if (parseInt(response.statusCode) !== 200)
+        {
+            //console.log("erro: "+ response.statusCode);
             listaErro.push(url);
-            listaRef.pop();
+            listaNormal.pop();
         }
         let domain = /\/\/(?:www.)?(.*?)(?:\/|"|')/.exec(url)[1];
         let fulldomain = /(http.?:\/\/(?:www.)?.*?)(?:\/|"|')/.exec(url)[1];
         
         let r = new RegExp("(?:\"|')((?:http.*?(?:"+domain+")|\\/)(?:[^.'\",\\s<>])*?(?:.html|\/|(?:[^.'\",\\s<>]{4,})))(?:\"|')", "gm");
 
-        while((result = r.exec(body)) !== null) {
-            let item = decodeURIComponent(result[1]);
-            //console.log(item);
-            if (item.startsWith("/")){
-                item = fulldomain + item;
-            }
-            if (item.startsWith("http") && !listaRef.includes(item)){
-                listaRef.push(item);
-                //console.log(item);
-                recReq(item, listaRef, listaErro);
-            }
-        }
-        if (cb)
-            cb(listaRef, listaErro);
+        execRequests(r, fulldomain, body, listaNormal, listaErro, function(listaNormal, listaErro){
+            if (cb)
+                cb(listaNormal, listaErro);
+        });
+        
     });
 }
 
@@ -50,8 +66,9 @@ function recReq(url, listaRef, listaErro, cb){
 //et domain = "cocacola.co.uk";
 //console.log(decodeURIComponent("http%3A%2F%2Fwww"))
 recReq('http://www.fantalatinamerica.com/es/home/', listaNormal, listaErro, function (l1, l2){
-    l1.sort(function(a, b){return a-b});
-    l2.sort(function(a, b){return a-b});
+    console.log("finalizando");
+    l1.sort();
+    l2.sort();
 
     l1.forEach(function(item){
         console.log(item);
